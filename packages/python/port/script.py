3d47571ce4b2c64095f6f290e57a55a7ca1b7b82
Frieder Rodewald <frod@frods-MBA.local>
2025-09-12 17:00:59 +0200

    implement check for valid instagram ddp

--- packages/python/port/script.py
+++ packages/python/port/script.py
@@ -1,19 +1,3 @@
-# --------------------------------------------------------------------
-# Note to developers:
-#
-# This script (`script.py`) provides a basic data donation flow using
-# standard UI components available in Feldspar.
-#
-# For a more advanced example that includes custom UI components
-# (e.g. a custom React-based component integrated with Python),
-# please refer to:
-#
-#     `script_custom_ui.py`
-#
-# That script demonstrates how to define and use your own components
-# using Feldspar’s React integration and how to render them via Python.
-# --------------------------------------------------------------------
-
 import port.api.props as props
 from port.api.assets import *
 from port.api.commands import CommandSystemDonate, CommandSystemExit, CommandUIRender
@@ -25,58 +9,87 @@ import time
 
 
 def process(sessionId):
-    key = "zip-contents-example"
-    meta_data = []
-    meta_data.append(("debug", f"{key}: start"))
+    locale = "de"
+    key = "instagram-data-donation-main-study"
 
     # STEP 1: select the file
     data = None
     while True:
-        meta_data.append(("debug", f"{key}: prompt file"))
-        promptFile = prompt_file("application/zip, text/plain")
+        # Allow users to only upload zip-files and render file-input page
+        promptFile = prompt_file("application/zip")
         fileResult = yield render_data_submission_page([promptFile])
-        if fileResult.__type__ == "PayloadString":
-            # Extracting the zipfile
-            meta_data.append(("debug", f"{key}: extracting file"))
-            extraction_result = []
-            zipfile_ref = get_zipfile(fileResult.value)
-            print(zipfile_ref, fileResult.value)
-            files = get_files(zipfile_ref)
-            fileCount = len(files)
-            for index, filename in enumerate(files):
-                percentage = ((index + 1) / fileCount) * 100
-                promptMessage = prompt_extraction_message(
-                    f"Extracting file: {filename}", percentage
-                )
-                yield render_data_submission_page(promptMessage)
-                file_extraction_result = extract_file(zipfile_ref, filename)
-                extraction_result.append(file_extraction_result)
 
-            if len(extraction_result) >= 0:
-                meta_data.append(
-                    ("debug", f"{key}: extraction successful, go to consent form")
-                )
-                data = extraction_result
-                break
-            else:
-                meta_data.append(
-                    ("debug", f"{key}: prompt confirmation to retry file selection")
-                )
-                retry_result = yield render_data_submission_page(retry_confirmation())
-                if retry_result.__type__ == "PayloadTrue":
-                    meta_data.append(("debug", f"{key}: skip due to invalid file"))
-                    continue
-                else:
-                    meta_data.append(("debug", f"{key}: retry prompt file"))
+        if fileResult.__type__ == "PayloadString":
+            # Check if valid Instagram DDP
+            check_ddp = check_if_valid_instagram_ddp(fileResult.value)
+
+            if check_ddp == "valid":
+                # Extracting the zipfile
+                extraction_result = []
+                zipfile_ref = get_zipfile(fileResult.value)
+                print(zipfile_ref, fileResult.value)
+                files = get_files(zipfile_ref)
+                fileCount = len(files)
+                for index, filename in enumerate(files):
+                    percentage = ((index + 1) / fileCount) * 100
+                    promptMessage = prompt_extraction_message(
+                        f"Extracting file: {filename}", percentage
+                    )
+                    yield render_data_submission_page(promptMessage)
+                    file_extraction_result = extract_file(zipfile_ref, filename)
+                    extraction_result.append(file_extraction_result)
+
+                if len(extraction_result) >= 0:
+                    data = extraction_result
                     break
+                else:
+                    retry_result = yield render_data_submission_page(retry_confirmation())
+                    if retry_result.__type__ == "PayloadTrue":
+                        continue
+                    else:
+                        break
+
+
+            elif (
+                check_ddp == "invalid_no_json"
+            ):
+                print(check_ddp)
+                
+                retry_result = yield render_data_submission_page(
+                    retry_confirmation_no_json()
+                )
+
+                if retry_result.__type__ == "PayloadTrue":
+                    continue
+
+            elif check_ddp == "invalid_no_ddp":
+                print(check_ddp)
+                
+                # Use platform-specific error messages
+                retry_result = yield render_data_submission_page(
+                    retry_confirmation_no_ddp()
+                )
+
+                if retry_result.__type__ == "PayloadTrue":
+                    continue
+
+            else:
+                
+                print(check_ddp)
+                retry_result = yield render_data_submission_page(
+                    retry_confirmation_no_ddp()
+                )
+
+                if retry_result.__type__ == "PayloadTrue":
+                    continue
+
+
 
     # STEP 2: ask for consent
-    meta_data.append(("debug", f"{key}: prompt consent"))
     for prompt in prompt_consent(data):
         result = yield prompt
         if result.__type__ == "PayloadJSON":
-            meta_data.append(("debug", f"{key}: donate consent data"))
-            meta_frame = pd.DataFrame(meta_data, columns=["type", "message"])
+            meta_frame = pd.DataFrame([1], columns=["type", "message"])
             data_submission_data = json.loads(result.value)
             data_submission_data["meta"] = meta_frame.to_json()
             yield donate(f"{sessionId}-{key}", json.dumps(data_submission_data))
@@ -84,16 +97,56 @@ def process(sessionId):
             value = json.dumps('{"status" : "data_submission declined"}')
             yield donate(f"{sessionId}-{key}", value)
 
+def check_if_valid_instagram_ddp(filename):
+    """Check if the uploaded file is a valid Instagram data download package"""
+    folder_name_check_ddp = "ads_information"
+    file_name_check_html = "start_here.html"
+
+    try:
+        with zipfile.ZipFile(filename, "r") as zip_ref:
+            found_folder_name_check_ddp = False
+            found_file_name_check_html = False
+
+            for file_info in zip_ref.infolist():
+                if folder_name_check_ddp in file_info.filename:
+                    found_folder_name_check_ddp = True
+
+                if file_name_check_html in file_info.filename:
+                    found_file_name_check_html = True
+
+            if found_folder_name_check_ddp:
+                if found_file_name_check_html:
+                    print(
+                        f"Folder '{folder_name_check_ddp}' found and file '{file_name_check_html}' found in the ZIP file. Seems like a Instagram HTML DDP."
+                    )
+                    return "invalid_no_json"
+
+                else:
+                    print(
+                        f"Folder '{folder_name_check_ddp}' found and file '{file_name_check_html}' not found in the ZIP file. Seems like a real Instagram JSON DDP."
+                    )
+                    return "valid"
+
+            else:
+                print(
+                    f"Folder '{folder_name_check_ddp}' not found. Does not seem like an Instagram DDP."
+                )
+                return "invalid_no_ddp"
+
+    except zipfile.BadZipFile:
+        print("Invalid ZIP file.")
+        return "invalid_file_zip"
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "invalid_file_error"
+
 
 def render_data_submission_page(body):
     header = props.PropsUIHeader(
         props.Translatable(
             {
-                "en": "Data donation flow example",
-                "de": "Beispiel für einen Datenspende-Ablauf",
-                "it": "Esempio di flusso di donazione dei dati",
-                "es": "Ejemplo de flujo de donación de datos",
-                "nl": "Voorbeeld van een datadonatieproces",
+                "de": "Instagram Datenspende",
             }
         )
     )
@@ -107,36 +160,63 @@ def render_data_submission_page(body):
 def retry_confirmation():
     text = props.Translatable(
         {
-            "en": "Unfortunately, we cannot process your file. Continue, if you are sure that you selected the right file. Try again to select a different file.",
             "de": "Leider können wir Ihre Datei nicht bearbeiten. Fahren Sie fort, wenn Sie sicher sind, dass Sie die richtige Datei ausgewählt haben. Versuchen Sie, eine andere Datei auszuwählen.",
-            "it": "Purtroppo non possiamo elaborare il tuo file. Continua se sei sicuro di aver selezionato il file corretto. Prova a selezionare un file diverso.",
-            "es": "Lamentablemente, no podemos procesar su archivo. Continúe si está seguro de que ha seleccionado el archivo correcto. Intente seleccionar un archivo diferente.",
-            "nl": "Helaas, kunnen we uw bestand niet verwerken. Weet u zeker dat u het juiste bestand heeft gekozen? Ga dan verder. Probeer opnieuw als u een ander bestand wilt kiezen.",
         }
     )
     ok = props.Translatable(
         {
-            "en": "Try again",
             "de": "Erneut versuchen",
-            "it": "Riprova",
-            "es": "Inténtelo de nuevo",
-            "nl": "Probeer opnieuw",
         }
     )
     cancel = props.Translatable(
-        {"en": "Continue", "de": "Weiter", "it": "Continua", "es": "Continuar", "nl": "Verder"}
+        {"de": "Weiter"}
     )
     return props.PropsUIPromptConfirm(text, ok, cancel)
+
+def retry_confirmation_no_json():
+    text = props.Translatable(
+        {
+            "de": 'Leider können wir Ihre Datei nicht verarbeiten. Es scheint so, dass Sie aus Versehen die HTML-Version Ihrer Instagram-Daten beantragt haben.\nBitte beantragen Sie erneut eine Datenspende bei Instagram und wählen Sie dabei "JSON" als Dateivormat aus (wie in der Anleitung beschrieben).'
+        }
+    )
+
+    ok = props.Translatable(
+        {
+            "de": "Erneut versuchen mit richtigen Daten"
+        }
+    )
+
+    cancel = props.Translatable(
+        {"de": "Weiter"}
+    )
+
+    return props.PropsUIPromptConfirm(text, ok, cancel)
+
+
+def retry_confirmation_no_ddp():
+    text = props.Translatable(
+        {
+            "de": f"Leider können wir Ihre Datei nicht verarbeiten. Haben Sie wirklich Ihre Instagram-Daten ausgewählt?"
+        }
+    )
+
+    ok = props.Translatable(
+        {"de": "Erneut versuchen"}
+    )
+
+    cancel = props.Translatable(
+        {"de": "Weiter"}
+    )
+
+    return props.PropsUIPromptConfirm(text, ok, cancel)
+
+
 
 
 def prompt_file(extensions):
     description = props.Translatable(
         {
-            "en": "Please select a zip file stored on your device.",
             "de": "Bitte wählen Sie eine ZIP-Datei auf Ihrem Gerät aus.",
-            "it": "Seleziona un file ZIP memorizzato sul tuo dispositivo.",
-            "es": "Por favor, seleccione un archivo ZIP guardado en su dispositivo.",
-            "nl": "Selecteer een ZIP-bestand dat op uw apparaat is opgeslagen.",
         }
     )
     
@@ -148,11 +228,7 @@ def prompt_file(extensions):
 def prompt_extraction_message(message, percentage):
     description = props.Translatable(
         {
-            "en": "One moment please. Information is now being extracted from the selected file.",
             "de": "Einen Moment bitte. Es werden nun Informationen aus der ausgewählten Datei extrahiert.",
-            "it": "Un momento, per favore. Le informazioni vengono estratte dal file selezionato.",
-            "es": "Un momento, por favor. Se están extrayendo los datos del archivo seleccionado.",
-            "nl": "Een moment geduld. Informatie wordt op dit moment uit het geselecteerde bestaand gehaald.",
         }
     )
 
@@ -187,22 +263,14 @@ def prompt_consent(data):
     description = props.PropsUIPromptText(
         text=props.Translatable(
             {
-                "en": "Please review your data below. Use the search fields to find specific information. You can remove any data you prefer not to share. Thank you for supporting this research project!",
                 "de": "Bitte überprüfen Sie Ihre Daten unten. Verwenden Sie die Suchfelder, um bestimmte Informationen zu finden. Sie können alle Daten entfernen, die Sie nicht teilen möchten. Vielen Dank für Ihre Unterstützung dieses Forschungsprojekts!",
-                "it": "Controlla i tuoi dati qui sotto. Usa i campi di ricerca per trovare informazioni specifiche. Puoi rimuovere qualsiasi dato che preferisci non condividere. Grazie per il tuo supporto a questo progetto di ricerca!",
-                "es": "Revise sus datos a continuación. Utilice los campos de búsqueda para encontrar información específica. Puede eliminar cualquier dato que prefiera no compartir. ¡Gracias por apoyar este proyecto de investigación!",
-                "nl": "Bekijk hieronder uw gegevens. Gebruik de zoekvelden om specifieke informatie te vinden. U kunt gegevens verwijderen die u liever niet deelt. Bedankt voor uw steun aan dit onderzoeksproject!",
             }
         )
     )
     
     table_title = props.Translatable(
         {
-            "en": "Zip file contents",
             "de": "Inhalt der ZIP-Datei",
-            "it": "Contenuto del file ZIP",
-            "es": "Contenido del archivo ZIP",
-            "nl": "Inhoud van het ZIP-bestand",
         }
     )
 
@@ -216,40 +284,24 @@ def prompt_consent(data):
             table_title,
             props.Translatable(
                 {
-                    "en": "The table below shows the contents of the zip file you selected.",
                     "de": "Die Tabelle unten zeigt den Inhalt der ZIP-Datei, die Sie gewählt haben.",
-                    "it": "La tabella qui sotto mostra il contenuto del file ZIP che ha scelto.",
-                    "es": "La tabla a continuación muestra el contenido del archivo ZIP que ha seleccionado.",
-                    "nl": "De tabel hieronder laat de inhoud zien van het zip-bestand dat u heeft gekozen.",
                 }
             ),
             data_frame,
             headers={
                 "filename": props.Translatable(
                     {
-                        "en": "Filename",
                         "de": "Dateiname",
-                        "it": "Nome del file",
-                        "es": "Nombre del archivo",
-                        "nl": "Bestandsnaam",
                     }
                 ),
                 "compressed_size": props.Translatable(
                     {
-                        "en": "Compressed Size",
                         "de": "Komprimierte Größe",
-                        "it": "Dimensione compressa",
-                        "es": "Tamaño comprimido",
-                        "nl": "Gecomprimeerde grootte",
                     }
                 ),
                 "size": props.Translatable(
                     {
-                        "en": "Uncompressed Size",
                         "de": "Dekomprimierte Größe",
-                        "it": "Dimensione non compressa",
-                        "es": "Tamaño descomprimido",
-                        "nl": "Ongecomprimeerde grootte",
                     }
                 ),
             },
@@ -261,20 +313,12 @@ def prompt_consent(data):
         2,
         props.Translatable(
             {
-                "en": "Example Metadata Table",
                 "de": "Beispieltabelle für Metadaten",
-                "it": "Tabella di metadati di esempio",
-                "es": "Tabla de metadatos de ejemplo",
-                "nl": "Voorbeeld van metagegevens tabel",
             }
         ),
         props.Translatable(
             {
-                "en": "This example table is included only to demonstrate that multiple tables can be shown. Its content is static and unrelated to your uploaded file.",
                 "de": "Diese Beispieltabelle zeigt, dass mehrere Tabellen angezeigt werden können. Ihr Inhalt ist statisch und steht in keinem Zusammenhang mit Ihrer hochgeladenen Datei.",
-                "it": "Questa tabella di esempio è inclusa solo per mostrare che è possibile visualizzare più tabelle. Il contenuto è statico e non è collegato al file caricato.",
-                "es": "Esta tabla de ejemplo se incluye solo para mostrar que se pueden mostrar varias tablas. Su contenido es estático y no está relacionado con su archivo cargado.",
-                "nl": "Deze voorbeeldtabel laat alleen zien dat er meerdere tabellen kunnen worden getoond. De inhoud is statisch en staat los van het geüploade bestand.",
   
             }
         ),
@@ -289,29 +333,17 @@ def prompt_consent(data):
         headers={
                 "Participant ID": props.Translatable(
                     {
-                        "en": "Participant ID",
                         "de": "Teilnehmer-ID",
-                        "it": "ID partecipante",
-                        "es": "ID del participante",
-                        "nl": "Deelnemer-ID",
                     }
                 ),
                 "Device": props.Translatable(
                     {
-                        "en": "Device",
                         "de": "Gerät",
-                        "it": "Dispositivo",
-                        "es": "Dispositivo",
-                        "nl": "Apparaat",
                     }
                 ),
                 "Date": props.Translatable(
                     {
-                        "en": "Date",
                         "de": "Datum",
-                        "it": "Data",
-                        "es": "Fecha",
-                        "nl": "Datum",
                     }
                 ),
             },
@@ -328,20 +360,12 @@ def prompt_consent(data):
                 props.PropsUIDataSubmissionButtons(
                     donate_question=props.Translatable(
                         {
-                            "en": "Would you like to donate the above data?",
                             "de": "Möchten Sie die obenstehenden Daten spenden?",
-                            "it": "Vuoi donare i dati sopra indicati?",
-                            "es": "¿Le gustaría donar los datos anteriores?",
-                            "nl": "Wilt u de bovenstaande gegevens doneren?",
                         }
                     ),
                     donate_button=props.Translatable(
                         {
-                            "en": "Yes, donate", 
                             "de": "Ja, spenden",
-                            "it": "Sì, dona",
-                            "es": "Sí, donar",
-                            "nl": "Ja, doneer",
                         }
                     ),
                 ),
